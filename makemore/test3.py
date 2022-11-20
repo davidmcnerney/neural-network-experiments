ba7e10b5052d95ae582e1a6f14c0fd61a5b3037f
David McNerney <davberk@gmail.com>
2022-11-19 20:04:19 -0500

    Complete basic neural net

--- makemore/test3.py
+++ makemore/test3.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Set
+from typing import Dict, List, Optional, Set
 
 import torch
 from torch.nn import functional as F
@@ -34,14 +34,37 @@ xenc = F.one_hot(xs, num_classes=27).float()
 ys = torch.tensor(y_list)
 yenc = F.one_hot(ys, num_classes=27).float()
 
-# Neuron
-# 5x27 input matrix multiplied by a 27x27 matrix yields 5x27 output matrix, where output rows correspond to input rows
-W = torch.randn((27, 27))
+# Create our one layer, with 27 neurons, one for each char
+generator = torch.Generator().manual_seed(2147483647)
+W = torch.randn((27, 27), generator=generator, requires_grad=True)
 
-# Forward pass
-logits = (xenc @ W)         # each col represents probability of that next char; we consider it log of predicted counts
-counts = logits.exp()       # after e**x operation, each col is proportional to predicted counts
-sums = counts.sum(dim=1, keepdims=True)   # collapses columns (dim 1) so that we get a 1 column matrix, whose value for each row is the sum of the elements in that row
-probs = counts / sums    # uses broadcasting to divide each column of each row by the one value in that row from the `sums` 5x1 matrix
+# Training
+loss_amount: Optional[float] = None
+for i in range(10000):
+    # Forward pass
 
-print("Done")
+    # Nx27 input matrix multiplied by a 27x27 matrix yields Nx27 output matrix, where output rows correspond to input rows
+    # exp and normalization so as to sum to 1, as we do below, is called "softmax"
+    logits = xenc @ W         # we interpret each col to be the log of predicted counts for corresponding char 0-27
+    counts = logits.exp()       # after e**x operation, each col is now proportional to predicted counts
+    sums = counts.sum(dim=1, keepdims=True)   # gives a Nx1 single column matrix, whose values are the sum of each row
+    # (sum collapses columns (dimension 1) across each row to yield a row sum)
+    probs = counts / sums       # now values in each row sum to exactly 1, and so are true relative probabilities
+    # when we scalar divide a Nx1 matrix into a Nx27 matrix, broadcasting divides each value in a row by the 1 value
+    # print(probs[0])
+
+    # Calculate loss - we measure loss by the average negative log likelihood. For each x,y sample, we look up
+    # the probability predicted by the network for the character y. If the network is predicting perfectly,
+    # it would be 1.0. We take the log, which in perfect conditions will therefore be 0.0. Since we'll never be perfect,
+    # we'll see probabilities < 1.0 which means negative logs, so we negate to get a positive number to minimize to 0.
+    loss = -probs[torch.arange(len(ys)), ys].log().mean()
+    loss_amount = loss.item()
+    # print(f"{i+1:3} loss: {loss.item()}")
+
+    # Backward pass
+    W.grad = None
+    loss.backward()
+    W.data += -0.1 * W.grad
+
+print(f"Final loss: {loss_amount}")
+
