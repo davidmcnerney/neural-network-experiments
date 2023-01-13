891f3cbd21e7c8114b7b1de944e4c02b19d13c68
David McNerney <davberk@gmail.com>
2023-01-13 13:46:36 -0500

    Some fixes for training

--- independent/gpt/running/test_running.py
+++ independent/gpt/running/test_running.py
@@ -10,32 +10,50 @@ def test_train():
     config = Configuration.nano()
     config.vocabulary_size = 4
     config.count_epochs = 1
-    config.batch_size = 4
+    config.batch_size = 2
     model = GPT(config=config)
-    dataset = torch.utils.data.TensorDataset(
-        torch.Tensor([                  # x
-            [100, 200, 300, 400],
-            [101, 201, 301, 401],
-        ]),
-        torch.Tensor([                  # y
-            [10, 20, 30, 40],
-            [11, 21, 31, 41],
-        ]),
-    )
+    x = torch.tensor([
+        [100, 200, 300, 400],
+        [101, 201, 301, 401],
+    ])
+    y = torch.tensor([
+        [10, 20, 30, 40],
+        [11, 21, 31, 41],
+    ])
+    dataset = torch.utils.data.TensorDataset(x, y)
     train(model=model, dataset=dataset)
 
 
 def test_forward():
     config = Configuration.nano()
+    config.batch_size = 1
     model = GPT(config=config)
 
-    batch_size = 1
     sequence_length = 4
     x = torch.tensor([[100, 200, 300, 400]])   # token indices: batch size x seq length
-    assert x.shape == torch.Size([batch_size, sequence_length])
-    y = model(x)
+    assert x.shape == torch.Size([config.batch_size, sequence_length])
 
-    assert y.shape == torch.Size([batch_size, sequence_length, config.vocabulary_size])
+    y = model(x)
+    assert y.shape == torch.Size([config.batch_size, sequence_length, config.vocabulary_size])
+
+    print("\n")
+    model.summarize_parameters()
+
+
+def test_forward_multiple_examples_in_batch():
+    config = Configuration.nano()
+    config.batch_size = 2
+    model = GPT(config=config)
+
+    sequence_length = 4
+    x = torch.tensor([  # token indices: batch size x seq length
+        [100, 200, 300, 400],
+        [101, 201, 301, 401],
+    ])
+    assert x.shape == torch.Size([config.batch_size, sequence_length])
+
+    y = model(x)
+    assert y.shape == torch.Size([config.batch_size, sequence_length, config.vocabulary_size])
 
     print("\n")
     model.summarize_parameters()
@@ -43,16 +61,16 @@ def test_forward():
 
 def test_generate():
     config = Configuration.nano()
+    config.batch_size = 1
     model = GPT(config=config)
 
-    batch_size = 1
     sequence_length = 4
     x = torch.tensor([[100, 200, 300, 400]])
-    assert x.shape == torch.Size([batch_size, sequence_length])
+    assert x.shape == torch.Size([config.batch_size, sequence_length])
     max_output_tokens = 3
-    y = model.generate(x, max_output_tokens=max_output_tokens)
 
-    assert y.shape == torch.Size([batch_size, sequence_length + max_output_tokens])
+    y = model.generate(x, max_output_tokens=max_output_tokens)
+    assert y.shape == torch.Size([config.batch_size, sequence_length + max_output_tokens])
 
     print("\n")
     print(y)
--- independent/gpt/running/training.py
+++ independent/gpt/running/training.py
@@ -1,5 +1,5 @@
 import statistics
-from typing import List, Tuple
+from typing import List, Set, Tuple
 
 import torch
 from torch import nn
@@ -16,13 +16,11 @@ def get_optimizer(model: GPT) -> torch.optim.Optimizer:
     and embedding weights.
     """
     parameters_requiring_weight_decay, parameters_not_requiring_weight_decay = _parameters_by_weight_decay_requirement(model)
-    print(f"parameters_requiring_weight_decay: {parameters_requiring_weight_decay}")
-    print(f"parameters_not_requiring_weight_decay: {parameters_not_requiring_weight_decay}")
     groups = [
         {"params": parameters_requiring_weight_decay, "weight_decay": model.config.weight_decay},
         {"params": parameters_not_requiring_weight_decay, "weight_decay": 0.0},
     ]
-    return torch.optim.AdamW(groups, learning_rate=model.config.learning_rate)
+    return torch.optim.AdamW(groups, lr=model.config.learning_rate)
 
 
 def train(
@@ -31,7 +29,7 @@ def train(
 ) -> None:
     # TODO: set num_workers for multiprocessing in data loader
     # TODO: make sure we are on the right devices everywhere
-    # All this code is in minGPT and nanoGPT
+    #    The code for ^ can be found in minGPT and nanoGPT
 
     optimizer = get_optimizer(model)
 
@@ -60,11 +58,15 @@ def train(
 
 
 def _parameters_by_weight_decay_requirement(model: GPT) -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
+    """
+    Bias parameters, layer normalization weights, and embedding weights are not to get weight decay.
+    """
+
     names_requiring_weight_decay = set()
     names_not_requiring_weight_decay = set()
     for module_name, module in model.named_modules():
         for parameter_name, _ in module.named_parameters():
-            full_parameter_name = f"{module_name}.{parameter_name}" if module_name is not None else parameter_name
+            full_parameter_name = f"{module_name}.{parameter_name}" if module_name else parameter_name
             if parameter_name.endswith("bias"):
                 names_not_requiring_weight_decay.add(full_parameter_name)
             elif parameter_name.endswith("weight"):
@@ -74,12 +76,16 @@ def _parameters_by_weight_decay_requirement(model: GPT) -> Tuple[List[torch.Tens
                     names_requiring_weight_decay.add(full_parameter_name)
                 else:
                     # We end up here when we encounter parameters in our container modules as we recursively descend,
-                    # because .named_parameters() is recursive.
+                    # because .named_parameters() is recursive. We'll eventually encounter each parameter in the context
+                    # of a LayerNorm, Embedding, or Linear Pytorch module, and categorize it then.
                     pass
             else:
                 raise Exception(f"Parameter {parameter_name} does not end with expected `weight` or `bias`")
 
-    parameters_by_name = {name: parameter for name, parameter in module.named_parameters()}
+    parameters_by_name = {name: parameter for name, parameter in model.named_parameters()}
+    # _summarize_set("names_requiring_weight_decay", names_requiring_weight_decay)
+    # _summarize_set("names_not_requiring_weight_decay", names_not_requiring_weight_decay)
+    # _summarize_set("parameters_by_name", set(parameters_by_name.keys()))
     if len(parameters_by_name) != len(names_requiring_weight_decay) + len(names_not_requiring_weight_decay):
         raise Exception
     requiring_weight_decay = [
@@ -92,3 +98,11 @@ def _parameters_by_weight_decay_requirement(model: GPT) -> Tuple[List[torch.Tens
     ]
 
     return requiring_weight_decay, not_requiring_weight_decay
+
+
+def _summarize_set(description: str, input_set: Set):
+    as_list = sorted(input_set)
+    print(f"\n{description} ({len(input_set)}):")
+    for item in as_list:
+        print(f"   {item}")
+    print("")
