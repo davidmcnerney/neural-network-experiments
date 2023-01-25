a058265b3b9d499d0f5464f124768ef3e8a65592
David McNerney <davberk@gmail.com>
2023-01-24 21:08:04 -0500

    training loss 1.3321 validation loss 7.7606

--- independent/gpt/projects/tiny_shakespeare/configuration.py
+++ independent/gpt/projects/tiny_shakespeare/configuration.py
@@ -6,18 +6,18 @@ def configuration() -> Configuration:
         vocabulary_size=5257,
         block_size=128,
 
-        embedding_size=128,
+        embedding_size=256,
         fully_connected_size=512,
         head_size=16,
 
         count_layers=4,
-        count_heads=8,
+        count_heads=16,
 
         embedding_dropout=0.2,
         attention_dropout=0.2,
         projection_dropout=0.2,
 
-        count_epochs=100,
+        count_epochs=1101,
         training_iterations_per_epoch=150,
         validation_iterations_per_epoch=25,
         batch_size=64,
