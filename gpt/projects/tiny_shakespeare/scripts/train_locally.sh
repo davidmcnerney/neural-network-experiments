38ebc2789e4ca1d1147b93533d0cb7990b6367c7
David McNerney <davberk@gmail.com>
2023-01-26 15:55:23 -0500

    Fix script

--- gpt/projects/tiny_shakespeare/scripts/train_locally.sh
+++ gpt/projects/tiny_shakespeare/scripts/train_locally.sh
@@ -5,4 +5,4 @@ VOCAB_FILENAME=~/Temp/gpt/bpe/tiny_shakespeare.5000.vocab.json
 MERGE_FILENAME=~/Temp/gpt/bpe/tiny_shakespeare.5000.merge.bpe
 MODEL_FILENAME=~/Temp/gpt/models/tiny_shakespeare.model
 
-PYTHONPATH=.  pipenv run python gpt/projects/tiny_shakespeare/train_tool.py --corpus-file "$CORPUS_FILENAME" --vocab-file "$VOCAB_FILENAME" --merge-file "$MERGE_FILENAME" --model-file "$MODEL_FILENAME"
+PYTHONPATH=.  pipenv run python gpt/projects/tiny_shakespeare/train_tool.py --corpus-file "$CORPUS_FILENAME" --vocab-file "$VOCAB_FILENAME" --merge-file "$MERGE_FILENAME" --model-file "$MODEL_FILENAME" "$@"
